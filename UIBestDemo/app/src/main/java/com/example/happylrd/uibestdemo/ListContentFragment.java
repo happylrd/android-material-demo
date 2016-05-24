b62256d061b6aeb8668d694e3fd332a430c39fdb
Diego Li <282108074@qq.com>
2016-05-24 18:28:06 +0800

    Add material design components
    Add Toolbar, Fragment and ViewPager

---
+++ UIBestDemo/app/src/main/java/com/example/happylrd/uibestdemo/ListContentFragment.java
@@ -0,0 +1,20 @@
+package com.example.happylrd.uibestdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lenovo on 2016/5/24.
+ */
+public class ListContentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_list, null);
+    }
+
+}
